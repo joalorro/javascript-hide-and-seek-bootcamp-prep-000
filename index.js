d5da6ba047c131e3b3a3ad44b03e1890d4a1a0f8
Josue V. Alorro <joalorro@gmail.com>
2018-06-15 21:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function getFirstSelector(selector) {
-  
-  return document.querySelector(selector)
-  
-}
-
-function nestedTarget( target ) {
-  
-  return document.getElementById('nested').getElementsByClassName('target')[0]
-  
-}
-
-var deepestChild = function () {
-  
-  return document.querySelector('div#grand-node div div div div')
-  
-}
 
 var increaseRankBy = function (n) {
   
