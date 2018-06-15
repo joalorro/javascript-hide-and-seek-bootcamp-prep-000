f45c14c908002154ddb1b51d23eb650bbc3b6450
Josue V. Alorro <joalorro@gmail.com>
2018-06-15 21:28:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function getFirstSelector(selector) {
+  
+  return document.querySelector(selector)
+  
+}
+
+function nestedTarget( target ) {
+  
+  return document.getElementById('nested').getElementsByClassName('target')[0]
+  
+}
+
+var deepestChild = function () {
+  
+  return document.querySelector('div#grand-node div div div div')
+  
+}
+
+var increaseRankBy = function (n) {
+  
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt( lis[i].innerHTML ) + n
+    
+    console.log(lis[i])
+  }
+}
+
+increaseRankBy(2)
